ae8127c1b38bdb8cfab2569a4c59dc6ea6193f02
Radu23c <cristea.radu23@gmail.com>
2025-08-08 15:31:19 +0300

    updates

--- src/components/Location/LocationMap.tsx
+++ src/components/Location/LocationMap.tsx
@@ -87,41 +87,6 @@ const LocationMap: React.FC<LocationMapProps> = ({
     he: ctaDescriptionHe,
   })
 
-  const getCategoryIcon = (category: string) => {
-    switch (category) {
-      case 'shopping':
-        return (
-          <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-            <path d="M7 4V2C7 1.45 7.45 1 8 1H16C16.55 1 17 1.45 17 2V4H20C20.55 4 21 4.45 21 5S20.55 6 20 6H19V19C19 20.1 18.1 21 17 21H7C5.9 21 5 20.1 5 19V6H4C3.45 6 3 5.55 3 5S3.45 4 4 4H7ZM9 3V4H15V3H9ZM7 6V19H17V6H7Z" />
-          </svg>
-        )
-      case 'education':
-        return (
-          <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-            <path d="M12 3L1 9L12 15L21 10.09V17H23V9M5 13.18V17.18L12 21L19 17.18V13.18L12 17L5 13.18Z" />
-          </svg>
-        )
-      case 'transport':
-        return (
-          <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-            <path d="M21 16V14L13 9V7.5C13 6.67 12.33 6 11.5 6S10 6.67 10 7.5V9L2 14V16L10 13.5V19L8 20.5V22L11.5 21L15 22V20.5L13 19V13.5L21 16Z" />
-          </svg>
-        )
-      case 'retail':
-        return (
-          <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-            <path d="M19 7H16V6A4 4 0 0 0 8 6V7H5A1 1 0 0 0 4 8V19A3 3 0 0 0 7 22H17A3 3 0 0 0 20 19V8A1 1 0 0 0 19 7M10 6A2 2 0 0 1 14 6V7H10V6M18 19A1 1 0 0 1 17 20H7A1 1 0 0 1 6 19V9H8V10A1 1 0 0 0 10 10A1 1 0 0 0 10 9V9H14V10A1 1 0 0 0 16 10A1 1 0 0 0 16 9V9H18V19Z" />
-          </svg>
-        )
-      default:
-        return (
-          <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-            <path d="M12 2C8.13 2 5 5.13 5 9C5 14.25 12 22 12 22S19 14.25 19 9C19 5.13 15.87 2 12 2M12 11.5C10.62 11.5 9.5 10.38 9.5 9S10.62 6.5 12 6.5S14.5 7.62 14.5 9S13.38 11.5 12 11.5Z" />
-          </svg>
-        )
-    }
-  }
-
   return (
     <section className="py-16 lg:py-24 bg-white">
       <div className="container mx-auto px-4">
@@ -197,10 +162,7 @@ const LocationMap: React.FC<LocationMapProps> = ({
                         index % 2 === 0 ? 'bg-gray-50' : 'bg-white'
                       } rounded-lg border border-gray-100 hover:border-[#D4B896] transition-all duration-300`}
                     >
-                      <div className="flex items-center space-x-4">
-                        <div className="text-[#D4B896] flex-shrink-0">
-                          {getCategoryIcon(point.category)}
-                        </div>
+                      <div className="flex items-center">
                         <span className="text-gray-900 font-medium text-lg">{pointName}</span>
                       </div>
                       <span className="text-[#D4B896] font-bold text-lg">{point.distance}</span>
