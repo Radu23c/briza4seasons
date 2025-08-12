995fa22b50737441a5546c8f26b4d4be18b54e38
radu-kallitone <radu@kallitone.com>
2025-08-12 18:41:27 +0300

    routes file in lib

---
+++ src/lib/routes.ts
@@ -0,0 +1,49 @@
+// lib/routes.ts
+export const routeTranslations = {
+  // Static pages
+  'about-us': {
+    ro: 'despre-noi',
+    en: 'about-us',
+    he: 'אודותינו',
+  },
+  contact: {
+    ro: 'contact',
+    en: 'contact',
+    he: 'צור-קשר',
+  },
+  'cookie-policy': {
+    ro: 'politica-cookie',
+    en: 'cookie-policy',
+    he: 'מדיניות-עוגיות',
+  },
+  gallery: {
+    ro: 'galerie',
+    en: 'gallery',
+    he: 'גלריה',
+  },
+  location: {
+    ro: 'locatie',
+    en: 'location',
+    he: 'מיקום',
+  },
+  posts: {
+    ro: 'articole',
+    en: 'posts',
+    he: 'פוסטים',
+  },
+  search: {
+    ro: 'cautare',
+    en: 'search',
+    he: 'חיפוש',
+  },
+  'terms-and-conditions': {
+    ro: 'termeni-si-conditii',
+    en: 'terms-and-conditions',
+    he: 'תנאים-והגבלות',
+  },
+  villas: {
+    ro: 'vile',
+    en: 'villas',
+    he: 'וילות',
+  },
+}
